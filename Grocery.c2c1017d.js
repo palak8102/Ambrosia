b48862d4d0d156bac67edbdc5eb59a9c8104bcb0
palak465 <113981479+palak465@users.noreply.github.com>
2025-06-19 00:42:36 +0530

    Updates

---
+++ Grocery.c2c1017d.js
@@ -0,0 +1,2 @@
+var e=globalThis.parcelRequire5906;(0,e.register)("iO0e0",function(r,o){Object.defineProperty(r.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(r.exports,"default",{get:()=>t,set:void 0,enumerable:!0,configurable:!0});var i=e("ayMG0"),t=()=>(0,i.jsxs)("h1",{children:["","Our Grocery online store!!!"]})});
+//# sourceMappingURL=Grocery.c2c1017d.js.map
